ae138ef9eeb0bbace345c3fb24296519baf75aa3
andrey <yaslonane@yandex.ru>
2017-06-25 20:48:43 +0300

    +

--- views/default/blog-single.php
+++ views/default/blog-single.php
@@ -7,9 +7,9 @@
                         <h2 class="page-title">BLOG PAGE</h2>
                         <div class="breadcrumbs">
                             <ul>
-                                <li>You're in : </li>
-                                <li>Home</li>
-                                <li>Blog</li>
+                                <li>Вы находитесь : </li>
+                                <li>Главная</li>
+                                <li>Блог</li>
                             </ul>
                         </div><!-- end breadcrumbs -->
                     </div><!-- end column -->
@@ -80,10 +80,6 @@
                                             <?php endforeach; ?>
                                         <?php endif; ?>
                                         </span><!-- end cat-links -->
-                                        <div class="comment-link">
-                                            <i class="fa fa-fw fa-comments"></i>
-                                            <a href="#">3 Comments</a>
-                                        </div>
                                     </div><!-- end entry-header -->
 
                                     <h1 class="entry-title">
@@ -121,9 +117,9 @@
                             </div><!-- end format-image hentry -->
 
                             <div class="author-wrapper">
-                                <h2>About Author</h2>
+                                <h2>Информация об авторе</h2>
                                 <figure class="author-ava">
-                                    <img src="images/content/auth-00.png" alt="">
+                                    <img src="<?php echo DOMAIN; ?>/images/content/auth-00.png" alt="">
                                 </figure>
                                 <div class="author-context">
 
@@ -139,12 +135,12 @@
                             </div><!-- end author-area -->
 
                             <div class="related-content">
-                                <h2>Related Content</h2>
+                                <h2>Похожие записи</h2>
                                 <div class="row">
 
                                     <div class="col-md-4">
                                         <figure>
-                                            <img src="images/content/blog-thumb-01.jpg" alt="">
+                                            <img src="<?php echo DOMAIN; ?>/images/content/blog-thumb-01.jpg" alt="">
                                         </figure>
                                         <div class="related-title">
                                             <h3><a href="#">Claritas est etiam processus dynamicus</a></h3>
@@ -153,7 +149,7 @@
 
                                     <div class="col-md-4">
                                         <figure>
-                                            <img src="images/content/blog-thumb-01.jpg" alt="">
+                                            <img src="<?php echo DOMAIN; ?>/images/content/blog-thumb-01.jpg" alt="">
                                         </figure>
                                         <div class="related-title">
                                             <h3><a href="#">Claritas est etiam processus dynamicus</a></h3>
@@ -162,7 +158,7 @@
 
                                     <div class="col-md-4">
                                         <figure>
-                                            <img src="images/content/blog-thumb-01.jpg" alt="">
+                                            <img src="<?php echo DOMAIN; ?>/images/content/blog-thumb-01.jpg" alt="">
                                         </figure>
                                         <div class="related-title">
                                             <h3><a href="#">Claritas est etiam processus dynamicus</a></h3>
@@ -172,155 +168,7 @@
                                 </div><!-- end row -->
                             </div><!-- end related-content -->
 
-                            <div class="comment-outer">
-                                <div id="comments" class="comments-area">
-                                    <h2 class="comments-title">4 COMMENTS</h2>
-                                    <ol class="comment-list">
-
-                                        <li class="comment"><!-- start comment -->
-                                            <div class="comment-body">
-
-                                                <figure class="comment-author vcard">
-                                                    <img src="images/content/auth-00.png" alt="">
-                                                </figure><!-- end comment-author vcard -->
-
-                                                <div class="comment-wrapper">
-                                                    <div class="comment-meta">
-                                                        <b class="fn">Jessica Marchetti</b>
-                                                        <div class="reply">
-                                                            <a href="#" class="comment-reply-link">Reply</a>
-                                                        </div>
-                                                    </div><!-- end comment-meta -->
-                                                    <div class="comment-metadata">
-                                                        <a href="#">
-                                                            <time datetime="2012-09-04T18:44:25+00:00">December 23 2014 pada 6:44 pm</time>
-                                                        </a>
-                                                    </div>
-                                                    <div class="comment-content">
-                                                        <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-                                                    </div><!-- end comment-content -->
-                                                </div><!-- end comment-wrapper -->
-
-                                            </div><!-- end comment-body -->
-                                        </li><!-- end li -->
-
-                                        <li class="comment"><!-- start comment -->
-                                            <div class="comment-body">
-
-                                                <figure class="comment-author vcard">
-                                                    <img src="images/content/auth-00.png" alt="">
-                                                </figure><!-- end comment-author vcard -->
-
-                                                <div class="comment-wrapper">
-                                                    <div class="comment-meta">
-                                                        <b class="fn">Paijo Kossim</b>
-                                                        <div class="reply">
-                                                            <a href="#" class="comment-reply-link">Reply</a>
-                                                        </div>
-                                                    </div><!-- end comment-meta -->
-                                                    <div class="comment-metadata">
-                                                        <a href="#">
-                                                            <time datetime="2012-09-04T18:44:25+00:00">December 23 2014 pada 6:44 pm</time>
-                                                        </a>
-                                                    </div>
-                                                    <div class="comment-content">
-                                                        <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-                                                    </div><!-- end comment-content -->
-                                                </div><!-- end comment-wrapper -->
-
-                                            </div><!-- end comment-body -->
-
-                                            <ol class="children">
-                                                <li class="comment"><!-- start-children-comment -->
-                                                    <div class="comment-body">
-
-                                                        <figure class="comment-author vcard">
-                                                            <img src="images/content/auth-00.png" alt="">
-                                                        </figure><!-- end comment-author vcard -->
-
-                                                        <div class="comment-wrapper">
-                                                            <div class="comment-meta">
-                                                                <b class="fn">Dina Makulatuwa</b>
-                                                                <div class="reply">
-                                                                    <a href="#" class="comment-reply-link">Reply</a>
-                                                                </div>
-                                                            </div><!-- end comment-meta -->
-                                                            <div class="comment-metadata">
-                                                                <a href="#">
-                                                                    <time datetime="2012-09-04T18:44:25+00:00">December 23 2014 pada 6:44 pm</time>
-                                                                </a>
-                                                            </div>
-                                                            <div class="comment-content">
-                                                                <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-                                                            </div><!-- end comment-content -->
-                                                        </div><!-- end comment-wrapper -->
-
-                                                    </div><!-- end comment-body -->
-                                                </li><!-- end comment -->
-                                            </ol><!-- end children -->
-                                        </li><!-- end li -->
-
-                                        <li class="comment"><!-- start comment -->
-                                            <div class="comment-body">
-
-                                                <figure class="comment-author vcard">
-                                                    <img src="images/content/auth-00.png" alt="">
-                                                </figure><!-- end comment-author vcard -->
-
-                                                <div class="comment-wrapper">
-                                                    <div class="comment-meta">
-                                                        <b class="fn">Johnny Kurniawan</b>
-                                                        <div class="reply">
-                                                            <a href="#" class="comment-reply-link">Reply</a>
-                                                        </div>
-                                                    </div><!-- end comment-meta -->
-                                                    <div class="comment-metadata">
-                                                        <a href="#">
-                                                            <time datetime="2012-09-04T18:44:25+00:00">December 23 2014 pada 6:44 pm</time>
-                                                        </a>
-                                                    </div>
-                                                    <div class="comment-content">
-                                                        <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-                                                    </div><!-- end comment-content -->
-                                                </div><!-- end comment-wrapper -->
-
-                                            </div><!-- end comment-body -->
-                                        </li><!-- end li -->
-
-                                    </ol><!-- end .comment-list -->
-                                </div><!-- end #comments-area -->
-
-                                <div id="respond" class="comment-respond">
-                                    <h2 id="reply-title" class="comment-reply-title">Leave a Reply</h2>
-                                    <form class="comment-form">
-                                        <p class="comment-notes">Your email address will not be published. All fields are required.</p>
-                                        <p class="comment-form-email">
-                                            <label for="author">Name</label>
-                                            <span class="required">*</span>
-                                            <input id="author" type="text" class="input-text" name="author">
-                                        </p>
-                                        <p class="comment-form-author">
-                                            <label for="email">Email</label>
-                                            <span class="required">*</span>
-                                            <input id="email" type="text" class="input-text" name="author">
-                                        </p>
-                                        <p class="comment-form-url">
-                                            <label for="url">Website</label>
-                                            <span class="required">*</span>
-                                            <input id="url" type="text" class="input-text" name="author">
-                                        </p>
-                                        <p class="comment-form-comment">
-                                            <label for="comment">Comment</label>
-                                            <textarea name="comment" id="comment" cols="45" rows="10" class="input-text"></textarea>
-                                        </p>
-                                        <p class="form-submit">
-                                            <input class="btn btn-lg btn-default" name="submit" type="submit" id="button" value="Post Comment">
-                                        </p>
-                                    </form><!-- end #comment-form -->
-                                </div><!-- end comment-respond -->
-
-                            </div><!-- end comment-outer -->
-
+                            
                         </div><!-- end #content -->
                         
                     </div><!-- end #primary -->
@@ -329,12 +177,12 @@
 
                         <div id="search-2" class="widget widget_search">
                             <div class="widget-title-outer">
-                                <h3 class="widget-title">Search</h3>
+                                <h3 class="widget-title">Поиск</h3>
                             </div>
                             <div class="searchform">
                                 <form>
-                                    <input type="text" class="txt" name="s" placeholder="Type Keywords">
-                                    <input type="submit" value="search" class="btn btn-sm">
+                                    <input type="text" class="txt" name="s" placeholder="Что хотите найти?">
+                                    <input type="submit" value="Найти" class="btn btn-sm">
                                 </form>
                             </div><!-- end searchform -->
                         </div><!-- end search widget -->
@@ -356,7 +204,7 @@
 
                             <div class="widget">
                                 <div class="widget-title-outer">
-                                    <h3 class="widget-title">News Tags</h3>
+                                    <h3 class="widget-title">Тэги</h3>
                                 </div>
                                 <div class="tagcloud">
                                     <a href="#">engine</a>
@@ -374,7 +222,7 @@
 
                             <div class="widget">
                                 <div class="widget-title-outer">
-                                    <h3 class="widget-title">Our Facebook Page</h3>
+                                    <h3 class="widget-title">Мы в контакте</h3>
                                 </div>
                                 <iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FFacebookDevelopers&amp;colorscheme=light&amp;show_faces=true&amp;header=true&amp;stream=false&amp;show_border=true"></iframe>
                             </div><!-- end widget -->
